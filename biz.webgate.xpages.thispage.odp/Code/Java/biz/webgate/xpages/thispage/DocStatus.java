95acbe6c455b55953c456509d30a721fa65355cc
Christian Guedemann <christian.guedemann@webgate.biz>
2014-04-18 14:51:13 +0200

    AbstractBase and Page

---
+++ biz.webgate.xpages.thispage.odp/Code/Java/biz/webgate/xpages/thispage/DocStatus.java
@@ -0,0 +1,5 @@
+package biz.webgate.xpages.thispage;
+
+public enum DocStatus {
+	DRAFT, PUBLISHED, OFFLINE;
+}
---
+++ biz.webgate.xpages.thispage.odp/Code/Java/biz/webgate/xpages/thispage/services/VersionHandler.java
@@ -0,0 +1,25 @@
+package biz.webgate.xpages.thispage.services;
+
+import com.ibm.xsp.extlib.util.ExtLibUtil;
+
+import lotus.domino.View;
+import lotus.domino.ViewEntryCollection;
+
+public enum VersionHandler {
+	INSTANCE;
+
+	public int getNewVersion(String docKey) {
+		int nResult = -1;
+		try {
+			View lupVersion = ExtLibUtil.getCurrentDatabase().getView("LUPVersion");
+			ViewEntryCollection vecVersion = lupVersion.getAllEntriesByKey(docKey, true);
+			if (vecVersion != null) {
+				int nVersion = (Integer) vecVersion.getFirstEntry().getColumnValues().elementAt(1);
+				nResult = nVersion++;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return nResult;
+	}
+}
