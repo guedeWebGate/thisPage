f0a57b62907ba5d2bd51b9012ec36a637fca4496
Christian Guedemann <christian.guedemann@webgate.biz>
2014-04-19 16:37:44 +0200

    Building Pages Overview

--- biz.webgate.xpages.thispage.odp/Code/Java/biz/webgate/xpages/thispage/content/CategoryStorageService.java
+++ biz.webgate.xpages.thispage.odp/Code/Java/biz/webgate/xpages/thispage/content/CategoryStorageService.java
@@ -4,9 +4,18 @@ import org.openntf.xpt.core.dss.AbstractStorageService;
 
 public class CategoryStorageService extends AbstractStorageService<Category> {
 
+	private static CategoryStorageService m_Service = new CategoryStorageService();
+
 	@Override
 	protected Category createObject() {
 		return new Category();
 	}
 
+	public static CategoryStorageService getInstance() {
+		return m_Service;
+	}
+
+	public String buildCategoryPath(String strKey) {
+		return strKey;
+	}
 }
