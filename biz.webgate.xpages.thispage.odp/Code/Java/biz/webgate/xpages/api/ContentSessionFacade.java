92df748926df0af90fb3f8b30e91f5b7d07cd1db
Christian Guedemann <christian.guedemann@webgate.biz>
2014-04-23 11:55:08 +0200

    SOS Requierments

--- biz.webgate.xpages.thispage.odp/Code/Java/biz/webgate/xpages/api/ContentSessionFacade.java
+++ biz.webgate.xpages.thispage.odp/Code/Java/biz/webgate/xpages/api/ContentSessionFacade.java
@@ -1,15 +1,21 @@
 package biz.webgate.xpages.api;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.faces.context.FacesContext;
 
+import org.openntf.xpt.core.dss.SingleObjectStore;
+
 import biz.webgate.xpages.thispage.DocStatus;
+import biz.webgate.xpages.thispage.content.Category;
+import biz.webgate.xpages.thispage.content.CategoryStorageService;
 import biz.webgate.xpages.thispage.content.Page;
 import biz.webgate.xpages.thispage.content.PageStorageService;
 import biz.webgate.xpages.thispage.services.UserNameProvider;
 
 public class ContentSessionFacade {
+	private static final String FRM_CATEGORY = "frmCategory";
 	private static final String FRM_PAGE = "frmPage";
 	private static final String LUP_ALL_EDITABLE_BY_FORM = "lupAllEditableByForm";
 	private static final String BEAN_NAME = "contentBean";
@@ -40,8 +46,26 @@ public class ContentSessionFacade {
 		PageStorageService.getInstance().save(pageDraft);
 		return pageDraft.getID();
 	}
+
 	public void setPageOffline(Page page) {
 		page.setStatus(DocStatus.OFFLINE);
 		PageStorageService.getInstance().save(page);
+	}
+
+	public SingleObjectStore<Page> getPageSOS() {
+		return new SingleObjectStore<Page>(PageStorageService.getInstance());
+	}
+
+	public void publishPage(Page page) {
+		PageStorageService.getInstance().publish(page);
+	}
+
+	public List<String> getCategoryList() {
+		List<Category> lstCat = CategoryStorageService.getInstance().getObjectsByForeignId(FRM_CATEGORY, LUP_ALL_EDITABLE_BY_FORM);
+		List<String> lstRC = new ArrayList<String>(lstCat.size());
+		for (Category cat : lstCat) {
+			lstRC.add(CategoryStorageService.getInstance().buildCategoryPath(cat.getDocKey()) + "|" + cat.getDocKey());
+		}
+		return lstRC;
 	}
 }
