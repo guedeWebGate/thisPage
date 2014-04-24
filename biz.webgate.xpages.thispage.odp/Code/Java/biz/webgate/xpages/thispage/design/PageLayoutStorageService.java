bed81854b6b398119a136ec6f1343cd89f6d2ca5
Christian Guedemann <christian.guedemann@webgate.biz>
2014-04-24 21:20:40 +0200

    PhotoPublisher optimized

--- biz.webgate.xpages.thispage.odp/Code/Java/biz/webgate/xpages/thispage/design/PageLayoutStorageService.java
+++ biz.webgate.xpages.thispage.odp/Code/Java/biz/webgate/xpages/thispage/design/PageLayoutStorageService.java
@@ -1,11 +1,17 @@
 package biz.webgate.xpages.thispage.design;
 
-import org.openntf.xpt.core.dss.AbstractStorageService;
+import biz.webgate.xpages.thispage.AbstractContentStorageService;
 
-public class PageLayoutStorageService extends AbstractStorageService<PageLayout> {
+public class PageLayoutStorageService extends AbstractContentStorageService<PageLayout> {
+
+	private static final PageLayoutStorageService m_Service = new PageLayoutStorageService();
+
+	public static PageLayoutStorageService getInstance() {
+		return m_Service;
+	}
 
 	@Override
-	protected PageLayout createObject() {
+	public PageLayout createObject() {
 		return new PageLayout();
 	}
 
--- biz.webgate.xpages.thispage.odp/Code/Java/biz/webgate/xpages/thispage/services/html/PhotoPublisher.java
+++ biz.webgate.xpages.thispage.odp/Code/Java/biz/webgate/xpages/thispage/services/html/PhotoPublisher.java
@@ -1,11 +1,16 @@
 package biz.webgate.xpages.thispage.services.html;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.imgscalr.Scalr;
 import org.openntf.xpt.core.utils.FileService;
 
 import biz.webgate.xpages.thispage.DocStatus;
@@ -25,14 +30,23 @@ public enum PhotoPublisher {
 			OutputStream os = response.getOutputStream();
 			if (pic != null) {
 				is = FileService.INSTANCE.getFileStream(pic.getFile().get(0));
-				StreamUtil.copyStream(is, os);
-				is.close();
+				if (blThumbnail) {
+					InputStream is2 = doResize(is, pic.getType(), 150);
+					is.close();
+					StreamUtil.copyStream(is2, os);
+					is2.close();
+				} else {
+					StreamUtil.copyStream(is, os);
+					is.close();
+
+				}
 			}
 			os.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
+
 	public void processToStreamByID(HttpServletRequest request, HttpServletResponse response, String strID) {
 		InputStream is = null;
 		Picture pic = PictureStorageService.getInstance().getById(strID);
@@ -41,13 +55,33 @@ public enum PhotoPublisher {
 			OutputStream os = response.getOutputStream();
 			if (pic != null) {
 				is = FileService.INSTANCE.getFileStream(pic.getFile().get(0));
-				StreamUtil.copyStream(is, os);
-				is.close();
+				if (blThumbnail) {
+					InputStream is2 = doResize(is, pic.getType(), 150);
+					is.close();
+					StreamUtil.copyStream(is2, os);
+					is2.close();
+				} else {
+					StreamUtil.copyStream(is, os);
+					is.close();
+				}
 			}
 			os.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	public InputStream doResize(InputStream is, String picType, int size) {
+		try {
+			BufferedImage bfi = ImageIO.read(is);
+			BufferedImage bfiTumb = Scalr.resize(bfi, size);
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			ImageIO.write(bfiTumb, picType, baos);
+			return new ByteArrayInputStream(baos.toByteArray());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 }
