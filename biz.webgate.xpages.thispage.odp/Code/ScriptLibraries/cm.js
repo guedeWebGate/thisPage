7261f870d20333d17688407d039199c5b8e24971
Christian Guedemann <christian.guedemann@webgate.biz>
2014-04-25 23:33:49 +0200

    Cleanup files not needed

--- biz.webgate.xpages.thispage.odp/Code/ScriptLibraries/cm.js
+++
@@ -1,8 +0,0 @@
-require([
-  "cm/lib/codemirror", "cm/mode/htmlmixed/htmlmixed"
-], function(CodeMirror) {
-  CodeMirror.fromTextArea(document.getElementById("code"), {
-    lineNumbers: true,
-    mode: "htmlmixed"
-  });
-});
